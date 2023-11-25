10d927b5174b7e50c0decf94bc62091ab8d93d4d
heypoppop <140745309+heypoppop@users.noreply.github.com>
2023-11-26 00:50:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for (int n : numbers) {
+            answer -= n;
+        }
+
+        return answer;
+    }
+}
