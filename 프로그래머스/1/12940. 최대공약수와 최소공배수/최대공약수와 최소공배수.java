986149205b596264acde3ded8dcc15a017bab0e0
heypoppop <140745309+heypoppop@users.noreply.github.com>
2023-12-07 11:57:32 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int a = Math.max(n, m);
+        int b = Math.min(n, m);
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return new int[] { a, n * m / a };
+    }
+}
