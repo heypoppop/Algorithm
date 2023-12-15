7ae51b03cb9598c5857f93a5631c755a7fa59da4
heypoppop <140745309+heypoppop@users.noreply.github.com>
2023-12-15 17:43:21 +0900

    [level 1] Title: 시저 암호, Time: 33.65 ms, Memory: 95 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == ' '){
+                answer+=ch;
+            }
+            if(ch >= 'a' && ch <= 'z'){
+                if(ch+n > 'z'){
+                    answer += (char)(ch - 26 + n);
+                }else{
+                    answer += (char)(ch + n);
+                }
+            }else if(ch >= 'A' && ch <='Z'){
+                if(ch+n > 'Z'){
+                    answer += (char)(ch - 26 + n);
+                }else{
+                    answer += (char)(ch + n);
+                }
+            }
+        }
+        return answer;
+    }
+}
