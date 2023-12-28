ee49910cbbf18de128fbb01a63586af2d85851a6
heypoppop <140745309+heypoppop@users.noreply.github.com>
2023-12-29 00:02:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 325.79 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+ 
+class Solution {
+        public String solution(String[] participant, String[] completion) {
+ 
+            Arrays.sort(participant);
+            Arrays.sort(completion);
+ 
+            int i = 0;
+            for(i = 0; i< completion.length; i++) {
+                if (!participant[i].equals(completion[i]))
+                    break;
+            }
+            return participant[i];
+    }
+}
