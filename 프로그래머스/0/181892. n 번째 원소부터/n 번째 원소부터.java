56d0297320edd211310734857b77f80b95434e54
heypoppop <140745309+heypoppop@users.noreply.github.com>
2024-02-01 23:22:14 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length - n + 1];
+            
+        answer = Arrays.copyOfRange(num_list, n - 1, num_list.length);
+
+        return answer;
+    }
+}
