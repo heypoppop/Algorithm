415acd49f2981aad8689fdadc5a4f377a52f1ae5
heypoppop <140745309+heypoppop@users.noreply.github.com>
2024-03-25 00:29:08 +0900

    [Bronze II] Title: 커트라인, Time: 280 ms, Memory: 21120 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+		Arrays.sort(arr);
+		System.out.println(arr[n - k]);
+		sc.close();
+	}
+}
