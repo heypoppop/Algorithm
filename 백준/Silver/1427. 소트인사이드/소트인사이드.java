d486991259d9792e1a100e4eb0768be3ec011561
heypoppop <140745309+heypoppop@users.noreply.github.com>
2024-04-15 06:54:56 +0900

    [Silver V] Title: 소트인사이드, Time: 228 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		char[] arr = in.nextLine().toCharArray();
+ 
+ 
+		Arrays.sort(arr);
+ 
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i]);
+		}
+ 
+	}
+}
