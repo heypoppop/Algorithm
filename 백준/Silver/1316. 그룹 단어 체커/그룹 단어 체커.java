01b039d45652fa4bebd4fef7b4693e5476819aa0
heypoppop <140745309+heypoppop@users.noreply.github.com>
2024-04-23 00:25:54 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); 
+        int count = N;
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+
+            int prev = 0;
+
+            int[] alphabet = new int[26];
+
+            for (int j = 0; j < str.length(); j++) {
+                int now = str.charAt(j);
+
+                if (prev != now) {
+                    if (alphabet[now - 97] == 0) {
+                        alphabet[now - 97]++;
+                        prev = now;
+                    } else {
+                        count--;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+}
